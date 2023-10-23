40c1707582e290f887e0840efd04f2c2466889e3
Ming Hwang Teh <ming.teh09@imperial.ac.uk>
2023-10-23 17:11:55 +0100

    Dinosaurs

--- api/app.py
+++ api/app.py
@@ -13,3 +13,14 @@ def submit():
     input_pet = request.form.get("pet")
     input_age = request.form.get("age")
     return render_template("confirm.html", pet=input_pet, age=input_age)
+
+@app.route("/query", methods=["GET"])
+def process_query(item):
+    if item == "dinosaurs":
+        my_string = "Dinosaurs ruled the Earth 200 million years ago"
+        return my_string
+    if item == "asteroids":
+        my_string = "Unknown"
+        return my_string
+
+
