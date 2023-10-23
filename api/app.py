0adb8162dfc5a414b3dadaba760823a485326033
Ming Hwang Teh <ming.teh09@imperial.ac.uk>
2023-10-23 16:10:11 +0100

    clean

--- api/app.py
+++ api/app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
-	return render_template("index.html")
+    return render_template("index.html")
+
 
 @app.route("/submit", methods=["POST"])
 def submit():
-	input_pet = request.form.get("pet")
-	input_age = request.form.get("age")
-	return render_template("confirm.html", pet=input_pet, age=input_age)
+    input_pet = request.form.get("pet")
+    input_age = request.form.get("age")
+    return render_template("confirm.html", pet=input_pet, age=input_age)
