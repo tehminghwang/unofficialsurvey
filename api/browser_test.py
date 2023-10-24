ceb29ad101db75496d6f1f518fee362d7700b25c
Ming Hwang Teh <ming.teh09@imperial.ac.uk>
2023-10-24 19:21:48 +0100

    new commits

--- api/browser_test.py
+++ api/browser_test.py
@@ -5,7 +5,7 @@ url = 'https://unofficialsurvey-nine.vercel.app'
 
 reqs = requests.get(url)
 
-soup = BeautifulSoup(reqs.text,'html.parser')
+soup = BeautifulSoup(reqs.text, 'html.parser')
 
 for title in soup.find_all('title'):
     assert(title.getText() == 'Unofficial Imperial Survey')
