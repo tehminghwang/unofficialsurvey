59145e2ad79f52759b4a03b75d0e47085a9af919
Ming Hwang Teh <ming.teh09@imperial.ac.uk>
2023-10-24 19:24:24 +0100

    new commits

--- api/browser_test.py
+++ api/browser_test.py
@@ -8,4 +8,4 @@ reqs = requests.get(url)
 soup = BeautifulSoup(reqs.text, 'html.parser')
 
 for title in soup.find_all('title'):
-    assert(title.getText() == 'Unofficial Imperial Survey')
+    assert title.getText() == 'Unofficial Imperial Survey'
