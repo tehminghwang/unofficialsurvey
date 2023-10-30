f073b93c61e13466cd09f9062aaf548cc1f15ad8
Ming Hwang Teh <ming.teh09@imperial.ac.uk>
2023-10-30 16:47:54 +0000

    new test

--- api/app.py
+++ api/app.py
@@ -25,13 +25,13 @@ def process_query(query_string):
         return "teamimperial"
     elif "largest" in query_string:
         numlist = re.findall(r'\d+', query_string)
-        return str(max(list(map(int, numlist))))
+        return int(max(list(map(int, numlist))))
     elif "plus" in query_string:
         numlist = re.findall(r'\d+', query_string)
-        return str(sum(list(map(int, numlist))))
+        return int(sum(list(map(int, numlist))))
     elif "multiplied" in query_string:
         numlist = re.findall(r'\d+', query_string)
-        return str(multiplyList(list(map(int, numlist))))
+        return int(multiplyList(list(map(int, numlist))))
     else:
         return "Query not recognised"
 
--- api/app_test.py
+++ api/app_test.py
@@ -13,16 +13,16 @@ def test_does_not_know_about_asteroids():
 
 
 def test_does_not_know_about_name():
-    assert process_query("name") == "teamimperial"
+    assert process_query("name") == "teamDOCSSE"
 
 
 def test_does_not_know_greatest_number():
-    assert process_query("largest: 45, 20, 73?") == "73"
+    assert process_query("largest: 45, 20, 73?") == 73
 
 
 def test_does_not_know_sum():
-    assert process_query("What is 86 plus 39?") == "125"
+    assert process_query("What is 86 plus 39?") == 125
 
 
 def test_does_not_know_multiply():
-    assert process_query("What is 34 multiplied by 14?") == "476"
+    assert process_query("What is 34 multiplied by 14?") == 476
